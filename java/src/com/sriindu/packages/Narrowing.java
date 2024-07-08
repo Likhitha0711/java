41f72e65ee345eb09f5dc752cbf9671c11767fd7
LIKHITHAREDDY <LIKHITHAREDDY@DESKTOP-3E56UMN>
2024-07-08 15:44:45 +0530

    commit message

---
+++ java/src/com/sriindu/packages/Narrowing.java
@@ -0,0 +1,14 @@
+package com.sriindu.packages;
+
+public class Narrowing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x=8.9;
+		int i=(int)x;
+		System.out.println("before modification "+x);
+		System.out.println("after modification "+i);
+
+	}
+
+}
---
+++ java/src/com/sriindu/packages/package-info.java
@@ -0,0 +1,2 @@
+package com.sriindu.packages;
+p
